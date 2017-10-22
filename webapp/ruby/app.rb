d64ed01d2b5afd9a3deb3fdc6d995b4aa8e65112
meganemura <meganemura@users.noreply.github.com>
2017-10-22 14:07:25 +0900

    デフォルトを帰る

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -354,7 +354,7 @@ class App < Sinatra::Base
     return @db_client if defined?(@db_client)
 
     @db_client = Mysql2::Client.new(
-      host: ENV.fetch('ISUBATA_DB_HOST') { 'localhost' },
+      host: ENV.fetch('ISUBATA_DB_HOST') { '127.0.0.1' },
       port: ENV.fetch('ISUBATA_DB_PORT') { '3306' },
       username: ENV.fetch('ISUBATA_DB_USER') { 'root' },
       password: ENV.fetch('ISUBATA_DB_PASSWORD') { '' },
